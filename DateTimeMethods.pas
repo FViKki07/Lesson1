81f74b289191a615ca95f8457ecce0a9a0bf856f
Viktory <fvd17rambled.ru@gmail.com>
2020-11-12 11:56:14 +0300

    add DaysInMonth

--- DateTimeMethods.pas
+++ DateTimeMethods.pas
@@ -1,7 +1,14 @@
 ﻿unit DateTimeMethods;
 
 function IsLeapYear(year: integer):= year mod 4=0;
-function DaysInMonth(month, year: integer): integer := 0;
+function DaysInMonth(month, year: integer): integer ;
+begin
+  case month of
+    1,3,5,7,8,10,12: Result:=31;
+    4,6,9,11: Result:=30;
+    2: if IsLeapYear(year) then REsult:=29 else Result:=28;
+  end;
+end;
 function LaterInDay(p1, p2: DateTime): DateTime := p1;
 function LaterInYear(p1, p2: DateTime): DateTime := p1;
 function DaysInYear(year: integer): integer := 0;
